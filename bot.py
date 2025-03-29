7c8499377b07f08ba5464f45749d39cc77813b4d
Shishkin PRO <viktorshish@gmail.com>
2025-03-29 20:24:36 +0300

    Conversarion Handler and logger add

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import os
 import logging
 import random
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 from environs import Env
 import redis
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters, Updater, ConversationHandler
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import (CallbackContext, CommandHandler, MessageHandler,
+                          Filters, Updater, ConversationHandler)
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 ANSWER = range(1)
@@ -57,85 +58,119 @@ def get_random_question(questions):
     return random.choice(list(questions.keys()))
 
 
+def get_keyboard():
+    return ReplyKeyboardMarkup([
+        ['Новый вопрос', 'Сдаться'],
+        ['Мой счет']
+    ])
+
+
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
-        text='Привет! Я бот для викторин! Чтобы начать, нажмите кнопку "Новый вопрос"',
-        reply_markup=ReplyKeyboardMarkup([
-            ['Новый вопрос'],
-            ['Мой счет']
-    ]))
+        text='Приветствую!\n\nЯ бот для викторин! Чтобы начать, нажмите кнопку "Новый вопрос"\n\n/cansel - для отмены',
+        reply_markup=get_keyboard()
+    )
 
 
 def handle_new_question_request(update, context):
     questions = context.bot_data['questions']
     question = get_random_question(questions)
-    answer = questions[question]
+    answer = questions[question].split('.\n', 1)[0].strip(' .!?')
 
     redis_db = context.bot_data['redis_db']
     chat_id = update.message.chat_id
-    redis_db.hset(chat_id, question, answer)
-    print(redis_db.hget(chat_id, question, '\n\n'))
-    # print(redis_db.get(chat_id), '\n\n', f'Ответ: {questions[redis_db.get(chat_id)]}')
+    current_score = redis_db.hget(chat_id, 'score') or 0
+    redis_db.hset(
+        chat_id,
+        mapping={
+            'current_question': question,
+            'current_answer': answer,
+            'score': current_score
+        },
+    )
+
+    logger.info(f'Current question: {redis_db.hgetall(chat_id)}')
 
     update.message.reply_text(
-        f'{question}\n\nВведите ответ:',
-        reply_markup=ReplyKeyboardRemove()
+        question,
+        reply_markup=get_keyboard()
     )
     return ANSWER
 
 
 def handle_solution_attempt(update: Update, context: CallbackContext):
-    questions = context.bot_data['questions']
-
-    answer = update.message.text.lower().strip(' .!?')
+    answer = update.message.text.lower().strip(' .!?\n')
 
     redis_db = context.bot_data['redis_db']
     chat_id = update.message.chat_id
 
-    question = redis_db.get(chat_id)
-    true_answer = questions[question].split('.\n', 1)[0].lower().strip(' .!?')
+    true_answer = redis_db.hget(chat_id, 'current_answer').lower()
 
     if answer in true_answer:
-        context.user_data['score'] += 1
-        print(context.user_data['score'])
+        redis_db.hincrby(chat_id, 'score', 1)
         update.message.reply_text(
-            'Правильно! Поздравляю!',
-            reply_markup=ReplyKeyboardMarkup([
-        ['Новый вопрос'],
-        ['Мой счет']
-    ]))
+            'Правильно! Поздравляю! Для следующего вопроса нажмите "Новый вопрос"',
+            reply_markup=get_keyboard()
+        )
         return ConversationHandler.END
 
     else:
         update.message.reply_text(
-            f'Неправильно. Попробуешь ещё раз?',
-            reply_markup=ReplyKeyboardMarkup([
-        ['Сдаться'],
-        ['Мой счет']
-    ])
+            f'Неправильно. Попробуй ещё раз.',
+            reply_markup=get_keyboard()
         )
         return ANSWER
 
 
 def handle_give_up(update: Update, context: CallbackContext):
+    redis_db = context.bot_data['redis_db']
+    chat_id = update.message.chat_id
+    true_answer = redis_db.hget(chat_id, 'current_answer')
+
     update.message.reply_text(
-        'Правильный ответ:  , Для нового вопроса нажмите "Новый вопрос"',
-    reply_markup=ReplyKeyboardMarkup([
-        ['Новый вопрос'],
-        ['Мой счет']
-    ]))
+        f'Правильный ответ: {true_answer}\n\nДля нового вопроса нажмите "Новый вопрос"',
+        reply_markup=get_keyboard()
+    )
     return ConversationHandler.END
 
 
 def handle_answer_dontknown(update: Update, context: CallbackContext):
     update.message.reply_text("Не понимаю")
+    return ConversationHandler.END
 
+
+def handle_cansel(update: Update, context: CallbackContext):
+    redis_db = context.bot_data['redis_db']
+    chat_id = update.message.chat_id
+    redis_db.delete(chat_id)
+
+    update.message.reply_text(
+        'Команда завершения работы викторины и обнуления счета'
+    )
+    return ConversationHandler.END
 
 def handle_get_score(update: Update, context: CallbackContext):
-    update.message.reply_text(f'Ваш счет - {context.bot_data["score"]}')
+    redis_db = context.bot_data['redis_db']
+    chat_id = update.message.chat_id
+    score = redis_db.hget(chat_id, 'score')
+    if not score:
+        score = 0
+    update.message.reply_text(
+        f'Ваш счет - {score} очков.\n\nДля продолжения, ответьте на заданный выше вопрос или нажмите "Новый вопрос".'
+    )
 
 
 def main():
+    formatter = logging.Formatter(
+        "%(name)s | %(levelname)s | %(asctime)s\n"
+        "%(message)s | %(filename)s:%(lineno)d",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(Path(__file__).parent / 'tg_bot.log', maxBytes=10000, backupCount=2)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
     env = Env()
     env.read_env()
 
@@ -168,18 +203,27 @@ def main():
         states={
             ANSWER: [
                 MessageHandler(Filters.regex('^(Сдаться)$'), handle_give_up),
-                MessageHandler(Filters.text, handle_solution_attempt),
+                MessageHandler(
+                    Filters.text & (~Filters.command),
+                    handle_solution_attempt
+                ),
             ],
         },
 
-        fallbacks=[MessageHandler(
-            Filters.video | Filters.photo | Filters.document | Filters.location,
-            handle_answer_dontknown
-        )],
+        fallbacks=[
+            CommandHandler('cancel', handle_cansel),
+            MessageHandler(
+                (Filters.video | Filters.photo | Filters.document | Filters.location),
+                handle_answer_dontknown
+            )
+        ]
     )
 
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.regex('^(Мой счет)$'), handle_get_score))
+    dp.add_handler(MessageHandler(
+        Filters.regex('^(Мой счет)$'),
+        handle_get_score)
+    )
     dp.add_handler(conv_handler)
 
     logger.info('Бот запущен')
